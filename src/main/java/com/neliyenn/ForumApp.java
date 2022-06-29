f8fffc4598eb7b54a449351842acc0d4defb974f
Neliyenn <aleksandraj.poczta@gmail.com>
2022-06-29 23:25:26 +0200

    zmiany w pliku ForumApp.java

--- src/main/java/com/neliyenn/ForumApp.java
+++ src/main/java/com/neliyenn/ForumApp.java
@@ -1,7 +1,12 @@
 package com.neliyenn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ForumApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SpringApplication.run(ForumApp.class, args);
     }
 }
