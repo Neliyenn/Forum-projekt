bf993848b86e706b169a2dd9386c1a8b0cb8e80c
Neliyenn <aleksandraj.poczta@gmail.com>
2022-10-15 20:10:55 +0200

    kosmetyka

--- src/main/resources/sql/import-h2.sql
+++ src/main/resources/sql/import-h2.sql
@@ -5,7 +5,7 @@ ALTER TABLE POST
 --wszystkie hasla jako zwykły tekst: "password"
 INSERT INTO USER (user_id, password, email, username, name, last_name, active)
 VALUES
-   (1, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'ramona@gmail.com', 'rama', 'Ramona', 'Pietruszewska', 1);
+    (1, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'ramona@gmail.com', 'rama', 'Ramona', 'Pietruszewska', 1);
 
 INSERT INTO USER (user_id, password, email, username, name, last_name, active)
 VALUES
